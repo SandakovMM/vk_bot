1c376bd4c737382fe8afbff5de46d981731c0729
Michail Sandakov <michail.sandakov@eltex.loc>
2019-04-18 14:20:24 +0700

    Make configuration read problems more readable

--- configuration.py
+++ configuration.py
@@ -2,10 +2,15 @@ import json
 
 class Configuration:
     def __init__(self, filename):
+        configuration = None
         try:
             with open(filename) as configuration_file:
                 configuration = json.load(configuration_file)
+        except:
+            print('Configuration file {} does not exist. Create it!'.format(filename))
+            exit(0)
 
+        try:
             self.api_vk_secret     = configuration['api_secret']
             self.confirm_vk_secret = configuration['confirm_secret']
             self.bot_module        = configuration['bot_module']
@@ -16,7 +21,7 @@ class Configuration:
 
             self.service_set       = configuration['service_set']
         except:
-            print('Configuration file {} does not exist. Create it!'.format(filename))
+            print('Configuration file {} does not store needed parameters.'.format(filename))
             exit(0)
 
     def get_confirm_secret(self):
